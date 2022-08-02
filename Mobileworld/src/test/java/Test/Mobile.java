3f8df3c4feaa5d74cd5a37f16e39fb6ab704ba48
edwin.kattumath <edwin.kattumath@QT-INBG-LT0475.QUALITEST.LOCAL>
2022-08-02 10:46:06 +0530

    contact

--- Mobileworld/src/test/java/Test/Mobile.java
+++ Mobileworld/src/test/java/Test/Mobile.java
@@ -1,5 +1,8 @@
 package Test;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -18,7 +21,7 @@ public class Mobile extends Generic {
 			driver.findElement(By.xpath("//input[@type='Password']")).sendKeys("Edwin@!123");//password
 			driver.findElement(By.xpath("//input[@type='date']")).sendKeys("14-06-1997");//DOB
 			driver.findElement(By.xpath("//label[.='Male']/../input[1]")).click();//Male
-			driver.findElement(By.xpath("//label[.='Female']/following-sibling::input")).click();//Female
+			//driver.findElement(By.xpath("//label[.='Female']/following-sibling::input")).click();//Female
 			driver.findElement(By.xpath("//input[@type='number']")).sendKeys("9847147279");//MOB
 			driver.findElement(By.xpath("//textarea[@placeholder='Short Bio']")).sendKeys("Hello Edwin Dixon");//BIO
 			driver.findElement(By.xpath("//button[@type='submit']")).click();//Register
@@ -41,9 +44,28 @@ public class Mobile extends Generic {
    		driver.findElement(By.xpath("//a[.='Log In']")).click();
     	
        }
-	
-	
        
+       
+       @Test
+       public void contactus()
+       {
+     	  driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
+          driver.findElement(By.linkText("Contact Us")).click();
+          Set<String> web=driver.getWindowHandles();
+          Iterator<String> it=web.iterator();
+          String parent_id=it.next();
+          String child_id=it.next();
+          driver.switchTo().window(child_id);
+          driver.findElement(By.xpath("//input[@type='text']")).sendKeys("EdwinDixon");
+          driver.findElement(By.xpath("//input[@type='email']")).sendKeys("edwindixon1@gmail.com");
+          driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("9847147279");
+          driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("Hello Mobileworld");
+          driver.findElement(By.xpath("//input[@value='Send']")).click();
+          driver.close();
+	
+       }
+       
+
 }
 
 	
